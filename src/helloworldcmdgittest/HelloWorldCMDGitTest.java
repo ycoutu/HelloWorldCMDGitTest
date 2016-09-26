58b13d1ffed6cfdaf384223185b38e9f476342f5
ycoutu <ysabellecoutu@gmail.com>
2016-09-26 16:43:16 -0400

    First commit

---
+++ src/helloworldcmdgittest/HelloWorldCMDGitTest.java
@@ -0,0 +1,12 @@
+
+package helloworldcmdgittest;
+
+//@ author Team Minion Patrol
+
+public class HelloWorldCMDGitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
