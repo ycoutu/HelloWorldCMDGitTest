4ab9e71d08f2207c851c9c9fbefe82c41e2ef97a
ycoutu <ysabellecoutu@gmail.com>
2016-09-26 17:08:38 -0400

    Add sexy new line
    A new line is now being printed in the console

--- src/helloworldcmdgittest/HelloWorldCMDGitTest.java
+++ src/helloworldcmdgittest/HelloWorldCMDGitTest.java
@@ -7,6 +7,7 @@ public class HelloWorldCMDGitTest {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("This is a line added by Bella!");
     }
     
 }
