52097af31187f5c0d779b1c530d1175a94c7e8b6
Suzariah <Suz2478@gmail.com>
2016-09-26 17:19:29 -0400

    Add new line
    A new line has been added

--- src/helloworldcmdgittest/HelloWorldCMDGitTest.java
+++ src/helloworldcmdgittest/HelloWorldCMDGitTest.java
@@ -8,6 +8,7 @@ public class HelloWorldCMDGitTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("This is a line added by Bella!");
+        System.out.println("This is also a new line by Suzanne!");
     }
     
 }
